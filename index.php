01926bd93c135b67b5a55e656123f2d7a665c035
Paul Rensing <prensing@ligerbots.org>
2016-08-13 08:35:47 -0700

    Checkin from actual website. Layout fixes
    Checkin from the hosted website. Directory is now git clone.
    Fix some layout problems on the home page.

--- index.php
+++ index.php
@@ -26,17 +26,19 @@
 	        <div class="orange-title">
 	          LIGERBOTS BLOG
 	        </div>
-                <div class="home-image-box">
+               <div id="blog-box">
+                 <div class="blog-image-box">
                   <?php echo find_first_image( $blog ); ?>
-	        </div>
+                 </div>
                 <div class="side-margins">
                   <?php
                      my_setup_postdata( $blog );
                      the_excerpt();
                      ?>
-                  <div class="read-more bottom-margin">
+                  <div class="read-more bottom-padding">
                     <?php echo '<a href="' . get_permalink( $blog ) . '">'; ?><img src="images/read_more.png"/></a>
                   </div>
+                 </div>
                 </div>
               </div>
             </div>
@@ -46,7 +48,7 @@
                 <div class="blue-title">
                   UPCOMING EVENTS
                 </div>
-                  <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;mode=AGENDA&amp;height=500&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=ligerbots.com_n95omorir7fj2bg2lu5q4ef8q0%40group.calendar.google.com&amp;color=%23711616&amp;ctz=America%2FNew_York"
+                  <iframe id="cal-iframe" src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;mode=AGENDA&amp;height=500&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=ligerbots.com_n95omorir7fj2bg2lu5q4ef8q0%40group.calendar.google.com&amp;color=%23711616&amp;ctz=America%2FNew_York"
                           width="100%" height="500" frameborder="0" scrolling="no">
                   </iframe>
               </div>
@@ -59,7 +61,7 @@
                 <div class="blue-title">
                   ANNOUNCEMENTS
                 </div>
-                <div class="blue-post side-margins" >
+                <div id="ann-box" class="blue-post side-margins" >
                   <?php
                      foreach ( get_announcements( 5 ) as $ann )
                      {
@@ -83,16 +85,16 @@
 	    <div class="col-md-6 bottom-margin">
 	      <div class="orange-border">
 	        <div class="orange-title">
-	          TWITTER
+                  TWITTER
 	        </div>
-                <a class="twitter-timeline" width="100%" href="https://twitter.com/LigerBots" data-widget-id="728971894213447680" data-chrome="noheader nofooter noborders">Tweets by @LigerBots</a>
+                <a id="twit-box" class="twitter-timeline" width="100%" href="https://twitter.com/LigerBots" data-widget-id="728971894213447680" data-chrome="noheader nofooter noborders">Tweets by @LigerBots</a>
                 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 	      </div>
 	    </div>
           </div>
 
           <div class="row wide-side-margins">
-            <div class="blue-border home-image-box">
+            <div class="blue-border brag-image-box">
               <img src="/images/team_photo_2015.jpg"/>
             </div>
             <div>
@@ -107,5 +109,21 @@
     </div>
 
     <?php page_foot(); ?>
+    <script>
+      function Resize( id1, id2 )
+      {
+          var s = $(id1);
+          var sh = s.height();
+          var t = $(id2);
+          var th = t.height();
+          if ( sh < th ) s.css( 'height', th );
+          else t.css( 'height', sh );
+      }
+
+      $(window).on('load', function() {
+         Resize( "#blog-box", "#cal-iframe" );
+         Resize( "#twitter-widget-0", "#ann-box" );
+      });
+    </script>
   </body>
 </html>
