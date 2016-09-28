828c9fa8a27e6cb2ace461f0e8b06734b287a661
Paul Rensing <prensing@cimetrics.com>
2016-09-27 22:49:10 -0400

    CSS for the calendar box on an iPhone.
    Also change the "read more" button to a flat version.

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ $blog = get_latest_blog();
                       my_the_excerpt( FALSE );
                       ?>
                       <div class="read-more">
-                        <a href="<?php echo get_permalink( $blog ); ?>"><img src="/images/read_more.svg"/></a>
+                        <a href="<?php echo get_permalink( $blog ); ?>"><img src="/images/read_more_flat.svg"/></a>
                       </div>
                     </div>
                   </div>
@@ -59,7 +59,7 @@ $blog = get_latest_blog();
                   <div class="panel-heading index-heading">
                     <a style="color:white;" href="/calendar.php">UPCOMING EVENTS</a>
                   </div>
-                  <div class="panel-body">
+                  <div id="cal-panel-div" class="panel-body">
                     <iframe id="cal-panel" src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;mode=AGENDA&amp;height=500&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=ligerbots.com_n95omorir7fj2bg2lu5q4ef8q0%40group.calendar.google.com&amp;color=%23711616&amp;ctz=America%2FNew_York"
                             width="100%" height="500" frameborder="0" scrolling="no">
                     </iframe>
