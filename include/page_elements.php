cb8937b83edcefcf4056714028ad6d164cca592a
Paul Renisng <prensing@ligerbots.org>
2022-06-14 21:15:41 -0400

    Update to 2022 sponsor images

--- include/page_elements.php
+++ include/page_elements.php
@@ -191,10 +191,10 @@ function output_footer( $sponsor_bar_name='home-sponsors' )
   <div class="col-xs-12">
     <div class="panel panel-sprs">
       <div class="big-sprs">
-        <embed class="sprs-image" src="/images/sponsor-logos/sponsor_bar_full_2021.svg" />
+        <embed class="sprs-image" src="/images/sponsor-logos/sponsor_bar_full_2022.svg" />
       </div>
       <div class="small-sprs">
-        <embed class="sprs-image" src="/images/sponsor-logos/sponsor_bar_narrow_2021.svg" />
+        <embed class="sprs-image" src="/images/sponsor-logos/sponsor_bar_narrow_2022.svg" />
       </div>
     </div>
     <div style="text-align: center;">
