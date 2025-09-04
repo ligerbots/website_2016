148862b4828ca9f0f24f90a9658cb26aecec76e7
Paul Renisng <prensing@ligerbots.org>
2025-09-04 12:34:53 -0400

    fix calendar on home page

--- index.php
+++ index.php
@@ -60,7 +60,7 @@ my_setup_postdata( $blog );
                     <a style="color:white;" href="/calendar.php">UPCOMING EVENTS</a>
                   </div>
                   <div id="cal-panel-div" class="panel-body">
-                    <iframe id="cal-panel" src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;mode=AGENDA&amp;height=500&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=c_r2405nbigo9eqb06kbvnk51bcc%40group.calendar.google.com&amp;color=%23711616&amp;ctz=America%2FNew_York"
+                    <iframe id="cal-panel" src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;mode=AGENDA&amp;height=500&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=c_ab7c91eb08574f09684076109ea998ec2df38bb839ff68399f801f6274446d76%40group.calendar.google.com&amp;color=%23711616&amp;ctz=America%2FNew_York"
                             width="100%" height="500" frameborder="0" scrolling="no">
                     </iframe>
                   </div>
